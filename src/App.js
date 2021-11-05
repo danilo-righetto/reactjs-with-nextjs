6739cd49343d7f89869293429f0f3af9a0d75988
Danilo Righetto <danilo.righetto@laborit.com.br>
2021-11-05 05:12:12 -0300

    state with arrays and objects

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.handlePClieck = this.handlePClieck.bind(this);
-    this.state = {
-      name: 'Danilo Righetto',
-      counter: 0
-    };
-  }
-
-  handlePClieck() {
-    const { name } = this.state;
-    console.log('O nome é: ' + name);
-    this.setState({ name: 'Ana' });
-  }
-
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
+  state = {
+    name: 'Danilo Righetto',
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'O titulo 1',
+        body: 'O corpo 1'
+      },
+      {
+        id: 2,
+        title: 'O titulo 2',
+        body: 'O corpo 2'
+      },
+      {
+        id: 3,
+        title: 'O titulo 3',
+        body: 'O corpo 3'
+      }
+    ]
+  };
 
   render() {
-    const { name, counter } = this.state;
+    const { name, counter, posts } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClieck}>
-            {name} {counter}
-          </p>
-          <a
-            onClick={this.handleAClick}
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {posts.map(post => <h1 key={post.id}>Teste: {post.title}</h1>)}
+
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
